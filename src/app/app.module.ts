94354a60d72d5ba8b862491c143613903adaaa23
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2020-08-06 22:33:41 +0530

    components removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { SecondComponent } from './second/second.component';
-import {ServerComponent} from './Server/Server.Component';
-import {ServersComponent} from './servers/servers.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SecondComponent,
-    ServerComponent,
-    ServersComponent,
     LoginComponent
   ],
   imports: [
